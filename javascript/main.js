00ab26c4a61ffa72fcd216ee51039adaecbc1389
lletycia <lletycia_costa@hotmail.com>
2019-05-27 21:40:39 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,13 @@
 let hamburger = document.querySelector(".navbar i");
 let menu = document.querySelector(".menu");
+let navBar = document.querySelector("#navbar");
+let sect = document.querySelector("#navbar li");
 
 function openMenu(e) {
     menu.classList.toggle("toggle");
     menu.style.transition = "500ms";
+    navBar.classList.toggle("toggle");
+    sect.classList.toggle("toggle");
 }
 
 function changeHamburger(e) {
